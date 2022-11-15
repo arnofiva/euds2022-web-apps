8d5bac934094ca632f31f1764feb70d33a602854
Arno Fiva <arnofiva@gmail.com>
2022-11-15 10:31:55 +0100

    Setup oauth and subscription

--- oauth/script.js
+++ oauth/script.js
@@ -4,18 +4,30 @@ import Expand from "https://js.arcgis.com/4.25/@arcgis/core/widgets/Expand.js";
 import WebScene from "https://js.arcgis.com/4.25/@arcgis/core/WebScene.js";
 import SceneView from "https://js.arcgis.com/4.25/@arcgis/core/views/SceneView.js";
 
-esriConfig.apiKey = "AAPK172864bc9b83465ca62bdb1118af560fije2SBSBx1VgYwU9-7VHnV2P4vTYU03PpxRXnMZIbbkiU1CHdegeNRA8j8lsqpjC";
+import IdentityManager from "https://js.arcgis.com/4.25/@arcgis/core/identity/IdentityManager";
+import OAuthInfo from "https://js.arcgis.com/4.25/@arcgis/core/identity/OAuthInfo";
+
+IdentityManager.registerOAuthInfos([
+  new OAuthInfo({
+    appId: "SSFsEgcHbBQWuQzo",
+    popup: true,
+    popupCallbackUrl: `${document.location.origin}${document.location.pathname}oauth-callback-api.html`,
+  }),
+]);
+
+window.setOAuthResponseHash = (responseHash) => {
+  IdentityManager.setOAuthResponseHash(responseHash);
+};
 
 const webScene = new WebScene({
   portalItem: {
-    id: "0b6dbaf6640540cfa83a3ba4cf97fb3f"
+    id: "74ea934f47de4e99807d260d4e69d9df"
   }
 });
 
 const view = new SceneView({
   map: webScene,
-  container: "viewDiv",
-  qualityMode: "high"
+  container: "viewDiv"
 });
 
 view.ui.add(
